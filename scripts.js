1cc954d04a1a9edac2c7d200ac1b11df6c83caed
Whovian79 <andrew.walsh001@student.swic.edu>
2020-03-03 20:16:56 -0600

    1st attempt at adding Car Rental. Allows blank field.

--- scripts.js
+++ scripts.js
@@ -10,5 +10,15 @@ console.log(name);
 alert(
   "Welcome, " +
     name +
-    "! All questions MUST completed with a whole number. If a cost does not apply to you, please enter '0'."
+    "! All questions MUST completed. If a cost does not apply to you, please enter '0'."
 );
+
+//Car Rental
+let rent = prompt("How much is your vehicle rental?");
+
+rent = Number(rent);
+
+while (rent !== 0 && !rent) {
+  alert("All fields must be completed with numbers only.");
+  rent = Number(prompt("How much is your vehicle rental?"));
+}
